587c153c94083395f1e12df6ebefd4228ad049a4
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-07-18 12:13:39 -0400

    initial commit and react skeleton

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Components/Navbar.js";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      hello world
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  // constructor(){
+  //   super()
+  // }
+
+  render() {
+    return <div>this is the navbar</div>;
+  }
+}
+
+export default Navbar;
